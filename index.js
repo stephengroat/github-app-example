9b96ae889fe95e204894a5fd55c9deebd0435fed
Stephen <stephen@egroat.com>
2018-01-17 09:04:41 -0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,42 @@
+var crypto = require('crypto')
+
+var createHandler = require('github-webhook-handler');
+
+var handler = createHandler({
+  path: '',
+  secret: 'eg4ZWoPdfRXrKNLtHuR7'
+});
+
+exports.createServer = function createServer(req, res) {
+  handler(req, res, function (err) {
+    console.error('Error:', err)
+    res.statusCode = 404
+    res.end('no such location')
+  });
+};
+
+var createApp = require('github-app');
+
+var app = createApp({
+  id: '8172',
+  cert: '123'
+});
+
+handler.on('error', function (err) {
+  console.error('Error:', err.message)
+});
+
+handler.on('issues', function (event) {
+  console.log("EVENT", event);
+  if (event.payload.action === 'opened') {
+    var installation = event.payload.installation.id;
+    app.asInstallation(installation).then(function (github) {
+      github.issues.createComment({
+        owner: event.payload.repository.owner.login,
+        repo: event.payload.repository.name,
+        number: event.payload.issue.number,
+        body: 'Welcome to the robot uprising.'
+      });
+    });
+  }
+});
